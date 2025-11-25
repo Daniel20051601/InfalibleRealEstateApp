69dbfc3aa6afb159de933b0834062af2840e08e5
Danielvi <danielvilopez@gmail.com>
2025-11-24 20:32:39 -0400

    Agrega la data class para manejar los estados de la Pantalla Catalogo

---
+++ app/src/main/java/com/infaliblerealestate/presentation/catalogo/CatalogoUiState.kt
@@ -0,0 +1,27 @@
+package com.infaliblerealestate.presentation.catalogo
+
+import com.infaliblerealestate.dominio.model.Propiedades
+
+data class CatalogoUiState(
+    val isLoading: Boolean = false,
+    val infoMessage: String? = null,
+    val propiedades: List<Propiedades> = emptyList(),
+    val propiedad: Propiedades? = null,
+    val propiedadId: Int? = null,
+    val showSheet: Boolean = false,
+    val showFilterDialog: Boolean = false,
+    val userMessage: String? = null,
+    val filtroCasa: Boolean = false,
+    val filtroDepartamento: Boolean = false,
+    val filtroVilla: Boolean = false,
+    val filtroPenthouse: Boolean = false,
+    val filtroSolar: Boolean = false,
+    val filtroLocalComercial: Boolean = false,
+    val filtroPrecio: Float = 0.0f,
+    val filtroHabitaciones: Int = 0,
+    val filtroBanos: Int = 0,
+    val filtroParqueos: Int = 0,
+    val filtroAlquiler: Boolean = false,
+    val filtroVenta: Boolean = false,
+    val showAllFilters: Boolean = false
+)
