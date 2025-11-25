2a992a26b3506fdb7ae34202292e71718be6289e
Danielvi <danielvilopez@gmail.com>
2025-11-24 20:37:35 -0400

    Mueve algunos archivos a otras carpetas para mejorar la estructura del proyecto

--- app/src/main/java/com/infaliblerealestate/data/remote/dto/UsuariosDto.kt
+++
@@ -1,8 +0,0 @@
-package com.infaliblerealestate.data.remote.dto
-
-data class UsuariosDto(
-    val usuarioId: Int?,
-    val userName: String,
-    val password: String
-)
-
--- app/src/main/java/com/infaliblerealestate/presentation/home/HomeEffect.kt
+++
@@ -1,5 +0,0 @@
-package com.infaliblerealestate.presentation.home
-
-sealed interface HomeEffect {
-    data object NavigateLogin : HomeEffect
-}
