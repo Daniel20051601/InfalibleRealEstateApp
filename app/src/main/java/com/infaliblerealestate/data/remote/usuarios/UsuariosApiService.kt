2fcea2fa6792873ad17c4b330a6b5cc0b43ef215
Danielvi <danielvilopez@gmail.com>
2025-11-26 17:56:51 -0400

    Crea la interfaz de la Api de Usuarios

---
+++ app/src/main/java/com/infaliblerealestate/data/remote/usuarios/UsuariosApiService.kt
@@ -0,0 +1,22 @@
+package com.infaliblerealestate.data.remote.usuarios
+
+import com.infaliblerealestate.data.remote.dto.login.LoginRequest
+import com.infaliblerealestate.data.remote.dto.usuarios.UsuarioRequest
+import com.infaliblerealestate.data.remote.dto.usuarios.UsuarioResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface UsuariosApiService {
+    @POST("api/Usuario/validar-credenciales")
+    suspend fun validarCredenciales(@Body credenciales: LoginRequest): Response<UsuarioResponse?>
+
+    @GET("api/Usuario/{id}")
+    suspend fun getUsuario(@Path("id") id : Int): Response<UsuarioResponse?>
+
+    @PUT("api/Usuario/{id}")
+    suspend fun putUsuario(@Path("id") id : Int, @Body usuario: UsuarioRequest): Response<UsuarioResponse>
+}
