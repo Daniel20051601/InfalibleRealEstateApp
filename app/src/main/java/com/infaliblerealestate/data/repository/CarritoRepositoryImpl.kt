32fc69cddb1bdf3536cb340d52b7d3cf847d7b8b
Danielvi <danielvilopez@gmail.com>
2025-11-26 18:24:14 -0400

    Crea la implementación del repositorio de Carrito

---
+++ app/src/main/java/com/infaliblerealestate/data/repository/CarritoRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package com.infaliblerealestate.data.repository
+
+import com.infaliblerealestate.data.mapper.toDomain
+import com.infaliblerealestate.data.remote.Resource
+import com.infaliblerealestate.data.remote.carrito.CarritoRemoteDataSource
+import com.infaliblerealestate.dominio.model.Carrito
+import com.infaliblerealestate.dominio.repository.CarritoRepository
+import javax.inject.Inject
+
+class CarritoRepositoryImpl @Inject constructor(
+    private val remoteDataSource: CarritoRemoteDataSource
+): CarritoRepository {
+    override suspend fun getCarritoByUserid(id: String): Resource<Carrito> {
+        return when(val result = remoteDataSource.getCarritoByUserid(id)){
+            is Resource.Success -> {
+                val carrito = result.data
+                Resource.Success(carrito?.toDomain())
+            }
+            is Resource.Error -> {
+                Resource.Error(result.message ?: "Error")
+            }
+            else -> {
+                Resource.Error("Error al obtener el carrito")
+            }
+        }
+
+    }
+
+    override suspend fun postCarrito(id: String, propiedadId: Int): Resource<Unit> {
+        return when(val result = remoteDataSource.postCarrito(id, propiedadId)){
+            is Resource.Success -> {
+                Resource.Success(Unit)
+            }
+            is Resource.Error -> {
+                Resource.Error(result.message ?: "Error")
+            }
+            else -> {
+                Resource.Error("Error al agregar el producto al carrito")
+            }
+        }
+    }
+}
