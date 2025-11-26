1dfdf7236cf65388167334c88eb579202e2b10c3
Danielvi <danielvilopez@gmail.com>
2025-11-26 17:52:29 -0400

    Agrega la clase UsuarioEntity para representar la entidad de usuario en la base de datos

---
+++ app/src/main/java/com/infaliblerealestate/data/local/entities/UsuarioEntity.kt
@@ -0,0 +1,18 @@
+package com.infaliblerealestate.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "usuarios")
+data class UsuarioEntity(
+    @PrimaryKey val id: String,
+    val userName: String,
+    val email: String,
+    val phoneNumber: String,
+    val nombre: String,
+    val apellido: String,
+    val estadoUsuario: String,
+    val rol: String,
+    val lastSync: Long = 0L
+)
+
