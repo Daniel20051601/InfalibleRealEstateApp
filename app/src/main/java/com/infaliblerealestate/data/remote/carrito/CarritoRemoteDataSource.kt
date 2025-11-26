6f3389d36f40baa70cdfe74bf454f2354478eb5c
Danielvi <danielvilopez@gmail.com>
2025-11-26 18:28:44 -0400

    Crea la clase CarritoRemoteDataSource para manejar las funciones de la Api de Carrito

---
+++ app/src/main/java/com/infaliblerealestate/data/remote/carrito/CarritoRemoteDataSource.kt
@@ -0,0 +1,37 @@
+package com.infaliblerealestate.data.remote.carrito
+
+import com.infaliblerealestate.data.remote.Resource
+import com.infaliblerealestate.data.remote.dto.carrito.CarritoResponse
+import javax.inject.Inject
+
+class CarritoRemoteDataSource @Inject constructor(
+    private val api: CarritoApiService
+) {
+    suspend fun getCarritoByUserid(id: String): Resource<CarritoResponse>{
+        return try {
+            val response = api.getCarritoByUserId(id)
+            if(response.isSuccessful){
+                response.body().let{Resource.Success(it)}
+            }else{
+                Resource.Error("Error ${response.code()}: ${response.message()}")
+            }
+        }catch (ex: Exception){
+            Resource.Error("Error: ${ex.localizedMessage}")
+        }
+    }
+
+    suspend fun postCarrito(id: String, propiedadId: Int): Resource<Unit>{
+        return try {
+            val response = api.postCarrito(id, propiedadId)
+            if(response.isSuccessful){
+                Resource.Success(Unit)
+            }else{
+                Resource.Error("Error ${response.code()}: ${response.message()}")
+            }
+        }catch (ex: Exception){
+            Resource.Error("Error: ${ex.localizedMessage}")
+        }
+    }
+
+}
+
