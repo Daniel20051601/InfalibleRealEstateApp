30236891e7bfec06d9a169450a4bba481c1cf381
Danielvi <danielvilopez@gmail.com>
2025-11-26 17:53:21 -0400

    Agrega la clase Usuario para representar el modelo de usuario en la aplicación

---
+++ app/src/main/java/com/infaliblerealestate/dominio/model/Usuario.kt
@@ -0,0 +1,13 @@
+package com.infaliblerealestate.dominio.model
+
+data class Usuario(
+    val id: String,
+    val userName: String,
+    val email: String,
+    val phoneNumber: String,
+    val nombre: String,
+    val apellido: String,
+    val estadoUsuario: String,
+    val rol: String
+)
+
