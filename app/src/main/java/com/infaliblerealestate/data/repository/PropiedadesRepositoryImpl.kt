890e961bf5d32f46f6e34733c1224bff5f099828
Danielvi <danielvilopez@gmail.com>
2025-11-30 18:42:29 -0400

    Implementa los métodos para crear, eliminar y obtener catálogos en el repositorio de propiedades

--- app/src/main/java/com/infaliblerealestate/data/repository/PropiedadesRepositoryImpl.kt
+++ app/src/main/java/com/infaliblerealestate/data/repository/PropiedadesRepositoryImpl.kt
@@ -4,6 +4,8 @@ import com.infaliblerealestate.data.mapper.toDomain
 import com.infaliblerealestate.data.mapper.toRequest
 import com.infaliblerealestate.data.remote.propiedades.PropiedadesRemoteDataSource
 import com.infaliblerealestate.data.remote.Resource
+import com.infaliblerealestate.dominio.model.Categorias
+import com.infaliblerealestate.dominio.model.EstadoPropiedad
 import com.infaliblerealestate.dominio.model.Propiedades
 import com.infaliblerealestate.dominio.repository.PropiedadesRepository
 import kotlinx.coroutines.flow.Flow
@@ -24,6 +26,7 @@ class PropiedadesRepositoryImpl @Inject constructor(
             else -> emit(emptyList())
         }
     }
+
     override suspend fun getPropiedad(id: Int): Resource<Propiedades> {
         return when (val result = remoteDataSource.getPropiedad(id)) {
             is Resource.Success -> {
@@ -52,6 +55,58 @@ class PropiedadesRepositoryImpl @Inject constructor(
             else -> {
                 Resource.Error("Error al actualizar la propiedad")
             }
+        }
+    }
+
+    override suspend fun postPropiedad(propiedad: Propiedades): Resource<Propiedades> {
+        return when(val result = remoteDataSource.postPropiedad(propiedad.toRequest())){
+            is Resource.Success -> {
+                val propiedad = result.data?.toDomain()
+                Resource.Success(propiedad)
+            }
+            is Resource.Error -> {
+                Resource.Error(result.message ?: "Error")
+            }
+            else -> {
+                Resource.Error("Error al crear la propiedad")
+            }
+
+        }
+    }
+
+    override suspend fun deletePropiedad(id: Int): Resource<Unit> {
+        return when(val result = remoteDataSource.deletePropiedad(id)){
+            is Resource.Success -> {
+                Resource.Success(Unit)
+            }
+            is Resource.Error -> {
+                Resource.Error(result.message ?: "Error")
+            }
+            else -> {
+                Resource.Error("Error al eliminar la propiedad")
+            }
+        }
+    }
+
+    override suspend fun getCategorias(): Flow<List<Categorias>> = flow{
+        when(val result = remoteDataSource.getCategorias()){
+            is Resource.Success -> {
+                val list = result.data?.map { it.toDomain() } ?: emptyList()
+                emit(list)
+            }
+            is Resource.Error -> {emit(emptyList())}
+            else -> emit(emptyList())
+        }
+    }
+
+    override suspend fun getEstadoPropiedades(): Flow<List<EstadoPropiedad>> = flow {
+        when(val result = remoteDataSource.getEstadoPropiedades()){
+            is Resource.Success -> {
+                val list = result.data?.map { it.toDomain() } ?: emptyList()
+                emit(list)
+            }
+            is Resource.Error -> { emit(emptyList()) }
+            else -> {emit(emptyList())}
         }
     }
 }
