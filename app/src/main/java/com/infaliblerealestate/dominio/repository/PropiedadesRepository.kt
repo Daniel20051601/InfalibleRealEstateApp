89591eae68cc26776bac8b6f5cb5a7a6c9376989
Danielvi <danielvilopez@gmail.com>
2025-11-30 18:45:01 -0400

    Agrega nuevas funciones a la interfaz del Repositorio de Propiedades

--- app/src/main/java/com/infaliblerealestate/dominio/repository/PropiedadesRepository.kt
+++ app/src/main/java/com/infaliblerealestate/dominio/repository/PropiedadesRepository.kt
@@ -1,6 +1,8 @@
 package com.infaliblerealestate.dominio.repository
 
 import com.infaliblerealestate.data.remote.Resource
+import com.infaliblerealestate.dominio.model.Categorias
+import com.infaliblerealestate.dominio.model.EstadoPropiedad
 import com.infaliblerealestate.dominio.model.Propiedades
 import kotlinx.coroutines.flow.Flow
 
@@ -8,4 +10,9 @@ interface PropiedadesRepository {
     suspend fun getPropiedades(): Flow<List<Propiedades>>
     suspend fun getPropiedad(id: Int): Resource<Propiedades>
     suspend fun putPropiedad(id: Int, propiedad: Propiedades): Resource<Unit>
+    suspend fun postPropiedad(propiedad: Propiedades): Resource<Propiedades>
+    suspend fun deletePropiedad(id: Int): Resource<Unit>
+    suspend fun getCategorias(): Flow<List<Categorias>>
+    suspend fun getEstadoPropiedades(): Flow<List<EstadoPropiedad>>
 }
+
