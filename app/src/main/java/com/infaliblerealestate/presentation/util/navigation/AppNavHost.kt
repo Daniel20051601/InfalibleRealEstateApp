c7dcd6c8b5b73863dee91d8dfcc4e71e5d88b6fe
Danielvi <danielvilopez@gmail.com>
2025-11-30 18:30:14 -0400

    Agrega nuevo screen al AppNavHost para manejar la creacion y edicion de propiedades

--- app/src/main/java/com/infaliblerealestate/presentation/util/navigation/AppNavHost.kt
+++ app/src/main/java/com/infaliblerealestate/presentation/util/navigation/AppNavHost.kt
@@ -2,7 +2,6 @@ package com.infaliblerealestate.presentation.util.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontVariation
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -13,6 +12,7 @@ import com.infaliblerealestate.presentation.catalogo.CatalogoScreen
 import com.infaliblerealestate.presentation.home.HomeScreen
 import com.infaliblerealestate.presentation.login.LoginScreen
 import com.infaliblerealestate.presentation.settings.SettingsScreen
+import com.infaliblerealestate.presentation.upsertPropiedad.UpsertPropiedadScreen
 
 @Composable
 fun AppNavHost(
@@ -25,15 +25,10 @@ fun AppNavHost(
         modifier = modifier
     ){
         composable(
-            route = Screen.Home.route,
-            arguments = listOf(
-                navArgument(Screen.Home.ARG) { type = NavType.StringType }
-            )
-        ) { backStackEntry ->
-            val id = backStackEntry.arguments?.getString(Screen.Home.ARG)
+            route = Screen.Home.route
+        ) {
             HomeScreen(
-                navController = navController,
-                usuarioId = id
+                navController = navController
             )
         }
 
@@ -63,7 +58,6 @@ fun AppNavHost(
         composable(
             route = Screen.Catalogo.route,
             arguments = listOf(
-                navArgument(Screen.Catalogo.ARG) { type = NavType.StringType },
                 navArgument(Screen.Catalogo.CATEGORIA_ARG) {
                     type = NavType.StringType
                     nullable = true
@@ -71,13 +65,35 @@ fun AppNavHost(
                 }
             )
         ) { backStackEntry ->
-            val id = backStackEntry.arguments?.getString(Screen.Catalogo.ARG)
             val categoria = backStackEntry.arguments?.getString(Screen.Catalogo.CATEGORIA_ARG)
             CatalogoScreen(
-                usuarioId = id,
-                categoriaInicial = categoria
+                categoriaInicial = categoria,
+                navController = navController,
             )
         }
+
+        composable(
+            route = Screen.UpsertPropiedad.route,
+            arguments = listOf(
+                navArgument(Screen.UpsertPropiedad.USUARIO_ID_ARG) {
+                    type = NavType.StringType
+                    nullable = true
+                },
+                navArgument(Screen.UpsertPropiedad.PROPIEDAD_ID_ARG) {
+                    type = NavType.IntType
+                    defaultValue = -1 // Usar -1 como valor por defecto para indicar "no presente"
+                }
+            )
+        ) { backStackEntry ->
+            val usuarioId = backStackEntry.arguments?.getString(Screen.UpsertPropiedad.USUARIO_ID_ARG)
+            val propiedadId = backStackEntry.arguments?.getInt(Screen.UpsertPropiedad.PROPIEDAD_ID_ARG)
+            UpsertPropiedadScreen(
+                usuarioId = usuarioId,
+                propiedadId = if (propiedadId == -1) null else propiedadId
+            )
+        }
+
+
 
         composable(Screen.Login.route) {
             LoginScreen(navController = navController)
