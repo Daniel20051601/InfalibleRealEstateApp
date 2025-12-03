2daaf51a54d95662e1e0b6330ad9b063df6a39e8
Danielvi <danielvilopez@gmail.com>
2025-12-02 23:45:35 -0400

    Refactor:♻️ Actualiza los parametros de los Screen para no usar directamente navController si no pasar una lambda

--- app/src/main/java/com/infaliblerealestate/presentation/util/navigation/AppNavHost.kt
+++ app/src/main/java/com/infaliblerealestate/presentation/util/navigation/AppNavHost.kt
@@ -1,5 +1,6 @@
 package com.infaliblerealestate.presentation.util.navigation
 
+import android.net.Uri
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -24,11 +25,21 @@ fun AppNavHost(
         startDestination = Screen.Home.route,
         modifier = modifier
     ){
-        composable(
-            route = Screen.Home.route
-        ) {
+        composable(route = Screen.Home.route) {
             HomeScreen(
-                navController = navController
+                onNavigateToCatalogo = { categoria ->
+                    navController.navigate(
+                        Screen.Catalogo.createRoute(Uri.encode(categoria))
+                    ) {
+                        popUpTo(Screen.Home.route) { saveState = true }
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToUpsertPropiedad = { usuarioId, propiedadId, isAdmin ->
+                    navController.navigate(
+                        Screen.UpsertPropiedad.createRoute(usuarioId, propiedadId, isAdmin)
+                    )
+                }
             )
         }
 
@@ -40,8 +51,12 @@ fun AppNavHost(
         ) { backStackEntry ->
             val id = backStackEntry.arguments?.getString(Screen.Settings.ARG)
             SettingsScreen(
-                navController = navController,
-                usuarioId = id
+                usuarioId = id,
+                onNavigateToLogin = {
+                    navController.navigate(Screen.Login.route) {
+                        popUpTo(0) { inclusive = true }
+                    }
+                }
             )
         }
 
@@ -68,7 +83,11 @@ fun AppNavHost(
             val categoria = backStackEntry.arguments?.getString(Screen.Catalogo.CATEGORIA_ARG)
             CatalogoScreen(
                 categoriaInicial = categoria,
-                navController = navController,
+                onNavigateToUpsertPropiedad = { usuarioId, propiedadId, isAdmin ->
+                    navController.navigate(
+                        Screen.UpsertPropiedad.createRoute(usuarioId, propiedadId, isAdmin)
+                    )
+                }
             )
         }
 
@@ -81,22 +100,35 @@ fun AppNavHost(
                 },
                 navArgument(Screen.UpsertPropiedad.PROPIEDAD_ID_ARG) {
                     type = NavType.IntType
-                    defaultValue = -1 // Usar -1 como valor por defecto para indicar "no presente"
+                    defaultValue = -1
+                },
+                navArgument("isAdmin") {
+                    type = NavType.BoolType
+                    defaultValue = false
                 }
             )
         ) { backStackEntry ->
             val usuarioId = backStackEntry.arguments?.getString(Screen.UpsertPropiedad.USUARIO_ID_ARG)
-            val propiedadId = backStackEntry.arguments?.getInt(Screen.UpsertPropiedad.PROPIEDAD_ID_ARG)
+            val propiedadId = backStackEntry.arguments?.getInt(Screen.UpsertPropiedad.PROPIEDAD_ID_ARG) ?.takeIf { it != -1 }
+            val isAdmin = backStackEntry.arguments?.getBoolean("isAdmin") ?: false
             UpsertPropiedadScreen(
                 usuarioId = usuarioId,
-                propiedadId = if (propiedadId == -1) null else propiedadId
+                propiedadId = propiedadId,
+                isAdmin = isAdmin,
+                onNavigateBack = {
+                    navController.popBackStack()
+                }
             )
         }
 
-
-
         composable(Screen.Login.route) {
-            LoginScreen(navController = navController)
+            LoginScreen(
+                onNavigateToHome = {
+                    navController.navigate(Screen.Home.createRoute()) {
+                        popUpTo(Screen.Login.route) { inclusive = true }
+                    }
+                }
+            )
         }
     }
 }
