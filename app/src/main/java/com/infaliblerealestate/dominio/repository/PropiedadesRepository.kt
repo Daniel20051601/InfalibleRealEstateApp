0b5e5c5f835b78c3d34ce7636bd2c211ec96408a
Danielvi <danielvilopez@gmail.com>
2025-12-02 23:30:43 -0400

    feat: ✨Agrega un nuevos metodos a la interfaz PropiedadesRepository para manejar la eliminacion de las imagenes y otro para obtener la lista de ubicaciones

--- app/src/main/java/com/infaliblerealestate/dominio/repository/PropiedadesRepository.kt
+++ app/src/main/java/com/infaliblerealestate/dominio/repository/PropiedadesRepository.kt
@@ -4,7 +4,9 @@ import com.infaliblerealestate.data.remote.Resource
 import com.infaliblerealestate.dominio.model.Categorias
 import com.infaliblerealestate.dominio.model.EstadoPropiedad
 import com.infaliblerealestate.dominio.model.Propiedades
+import com.infaliblerealestate.dominio.model.Ubicacion
 import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
 
 interface PropiedadesRepository {
     suspend fun getPropiedades(): Flow<List<Propiedades>>
@@ -14,5 +16,8 @@ interface PropiedadesRepository {
     suspend fun deletePropiedad(id: Int): Resource<Unit>
     suspend fun getCategorias(): Flow<List<Categorias>>
     suspend fun getEstadoPropiedades(): Flow<List<EstadoPropiedad>>
+    suspend fun uploadImages(propiedadId: Int, images: List<MultipartBody.Part>): Resource<Unit>
+    suspend fun deleteImages(imagenesIds: List<Int>): Resource<Unit>
+    suspend fun getUbicaciones(): Resource<Ubicacion>
 }
 
